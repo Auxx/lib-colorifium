14547dd7e61ac04ce85a54408689747b0682fcc4
Alexander 'Aux' Ivanov <aux@hexmode.org>
2018-01-27 19:48:18 +0000

    Added simple build task

---
+++ src/main/index.ts
@@ -0,0 +1,7 @@
+export * from './converters/hsl';
+export * from './converters/hsv';
+export * from './converters/rgb8';
+export * from './converters/rgbf';
+export * from './converters/xyz';
+
+export * from './interfaces/color-models';
